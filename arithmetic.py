f0e0ae512148f0a0831db3758fb4dbc73c1fa984
visay vang <vsvang@gmail.com>
2023-03-15 16:06:30 -0400

    first commit

---
+++ arithmetic.py
@@ -0,0 +1,67 @@
+"""Functions for common math operations."""
+
+
+def add(num1, num2):
+    """Return the sum of num1 and num2."""
+    #set an equation that sums num1, num2 
+    sum = num1 + num2
+
+    #return sum 
+    return sum
+
+
+def subtract(num1, num2):
+
+    difference = num1 - num2
+    """Return the value of num1 minus num2."""
+
+    return difference
+
+def multiply(num1, num2):
+    """Multiply the num1 by num2 and return the result."""
+    #variable called product and multiply num1 and num2 
+    product = num1 * num2
+
+    #return product
+    return product
+
+def divide(num1, num2):
+    """Divide the num1 by num2, returning a floating point."""
+    #variable called division and divide num1 and num2
+    division = num1 / num2
+
+    #return the value
+    return division
+
+
+def square(num1):
+    """Return the square of num1."""
+    #square_total = num1 * num1 
+    square_total = num1 * num1
+    
+    #return square_total
+    return square_total
+
+
+def cube(num1):
+    """Return the cube of num1."""
+    #cube_total = num1 * num1 * num1
+    cube_total = num1 * num1 * num1
+
+    #return cube total
+    return cube_total
+
+
+def power(num1, num2):
+    """Raise num1 to the power of num2 and return the value."""
+    power = num1 ** num2
+
+    return power
+
+
+def mod(num1, num2):
+    """Return the remainder of num1 / num2."""
+    mod_total = num1 % num2
+
+    #return mod total
+    return mod_total
